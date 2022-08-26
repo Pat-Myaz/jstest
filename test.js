732dc14c700ed93fe3d666ec63a254f63eab986b
Theo Bubach <theo.bubach@learnet.se>
2022-08-26 15:38:36 +0200

    filer

---
+++ test.js
@@ -0,0 +1,55 @@
+// skapa ett program som säger hello world
+
+console.log ("hello world")
+
+
+
+// skapa ett program som skriver ut once upon a time, i was falling in love, now i'm only falling apart
+
+console.log ("once upon a time")
+console.log ("i was falling in love")
+console.log ("c")
+
+
+// gör samma som innan men det ska vara uppdelat i tre rader
+
+console.log ("once upon a time \n i was falling in love \n now i'm only falling apart")
+
+
+// ändra texten så den skriver ut live and sleep
+
+
+var text = "live and sleep";
+console.log(text);
+
+
+//ändra texten till hej ada lovelace
+
+var username = "Hej Ada Lovelace";
+console.log (username);
+
+
+//be användaren skriva sitt namn
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("Enter your name: ");
+
+console.log (name)
+
+
+
+
+// skapa ett program som säger hej 3 gånger
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let Namn = prompt("Enter input: ");
+
+console.log (Namn)
+console.log (Namn)
+console.log (Namn)
+
+
